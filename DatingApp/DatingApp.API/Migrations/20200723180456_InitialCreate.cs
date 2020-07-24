349a1ac7e1b98102f443095852e3edeaf19eba22
Baden Terblanche <baden.terblanche@gmail.com>
2020-07-24 17:40:02 +0200

    Initial Commit

---
+++ DatingApp/DatingApp.API/Migrations/20200723180456_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Values",
+                columns: table => new
+                {
+                    ValueID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ValueName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Values", x => x.ValueID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Values");
+        }
+    }
+}
---
+++ DatingApp/DatingApp.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        public int ValueID { get; set; }
+        public string ValueName { get; set; }
+    }
+}
